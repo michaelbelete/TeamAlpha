cdcd55613e130997bddbf2054bf1a1755d2ede82
michaelbelete <it.michael.belete@gmail.com>
2019-10-25 14:37:54 -0800

    showing date using Date object
    must import the java.util.date;

---
+++ Programming/tutorial/src/DateShow.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class DateShow {
+    public static void main(String[] args){
+        Date myDate = new Date();
+        System.out.println("The date is " + myDate);
+    }
+}
