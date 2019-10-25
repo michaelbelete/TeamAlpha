e21af5b214b15a45386b08426478bd6d307b2101
michaelbelete <it.michael.belete@gmail.com>
2019-10-25 14:19:12 -0800

    created and printed out characters
    char are single quotes

---
+++ Programming/tutorial/src/Char.java
@@ -0,0 +1,13 @@
+public class Char {
+    public static void main(String[] args){
+        char c1 = '1';
+        char c2 = '2';
+        char c3 = '3';
+
+        char dollar = '\u0024'; //code found in unicode.org
+        System.out.print(dollar);
+        System.out.print(c1);
+        System.out.print(c2);
+        System.out.print(c3);
+    }
+}
