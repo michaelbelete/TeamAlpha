41ac8cb3ed123af3d9f107d1db51dcee60ddbee8
eban12 <it.eyob.alemu@gmail.com>
2019-10-25 05:46:56 -0700

    started a new calculator project
    the main menu has been done

---
+++ Programming/Practise/src/Calculator.java
@@ -0,0 +1,22 @@
+// simple console based calculator with java
+// @author Eyob Alemu
+
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args){
+		
+      int choice == -999;
+      Scanner sc = new Scanner(System.in);
+			
+      do{
+			
+        System.out.println("[1] for trig");
+        System.out.println("[2] for log"); 
+        System.out.println("[3] quit");
+        choice = sc.nextInt(); 
+				
+      }while(choice != 3);    
+    }
+}
+
