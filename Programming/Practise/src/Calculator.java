64ce090efbffdaa0001c8138e816623bf98738eb
eban12 <it.eyob.alemu@gmail.com>
2019-10-27 12:05:59 +0300

    method to accept inputs created
    it's static method that can be used to accept inputs by the whole class

--- Programming/Practise/src/Calculator.java
+++ Programming/Practise/src/Calculator.java
@@ -9,7 +9,17 @@ public class Calculator{
 	private static double result; // Holds every computation result
     private static double input; // accepts values to be computed
     private static boolean repeat = true; // weather to continue or not
-    private static Scanner sc = new Scanner(System.in); 
+    private static Scanner sc = new Scanner(System.in);
+	
+	private static void acceptInput(String message, String operation) {
+        // 'message' is info about what the user should insert
+        // 'operation' is the type of computation chosen by the user
+
+        System.out.printf("======[%s]======\n", operation);
+        System.out.printf("Enter %s...: ", message);
+        input = sc.nextDouble();
+        System.out.println();
+    }
 	
     public static void main(String[] args){	
       int choice;
