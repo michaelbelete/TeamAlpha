be5efac1dc46463d61a263288bddf9ee4817ef6d
michaelbelete <it.michael.belete@gmail.com>
2019-10-29 14:06:23 -0800

    styles the main menu using ascii
    http://patorjk.com/software/taag/#p=display&f=Alpha&t=TeamAlpha

--- Maths/Assignment/assignement.py
+++ Maths/Assignment/assignement.py
@@ -1,5 +1,12 @@
-print("***************************** Main Menu *****************************")
-print("Choose: ")
+print('''
+  _____                       _    _       _           
+ |_   _|__  __ _ _ __ ___    / \  | |_ __ | |__   __ _ 
+   | |/ _ \/ _` | '_ ` _ \  / _ \ | | '_ \| '_ \ / _` |
+   | |  __/ (_| | | | | | |/ ___ \| | |_) | | | | (_| |
+   |_|\___|\__,_|_| |_| |_/_/   \_\_| .__/|_| |_|\__,_|
+                                    |_|                
+''')
+print("Choose what you wanted to do: ")
 print('''
   1. Conjuction       6. Union            9.Power Set
   2. Disjunction      7. Intersection
@@ -7,4 +14,3 @@ print('''
   4. Bi conditional   9.Symmetric Difference 
   5. Negation         8. Complement
 ''')
-print("***************************** Main Menu *****************************")
