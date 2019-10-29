ffd21a9fa1b18fb9af66311610572085ce099b80
michaelbelete <it.michael.belete@gmail.com>
2019-10-29 13:52:35 -0800

    created a main manu

---
+++ Maths/Assignment/assignement.py
@@ -0,0 +1,10 @@
+print("***************************** Main Menu *****************************")
+print("Choose: ")
+print('''
+  1. Conjuction       6. Union            9.Power Set
+  2. Disjunction      7. Intersection
+  3. Implication      8. Difference
+  4. Bi conditional   9.Symmetric Difference 
+  5. Negation         8. Complement
+''')
+print("***************************** Main Menu *****************************")
