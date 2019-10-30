bf423ef939879f08f18668f896dfc45fe4fd3c28
michaelbelete <it.michael.belete@gmail.com>
2019-10-30 14:12:16 -0800

    using loop in java
    this file contain

---
+++ Programming/tutorial/src/loop.java
@@ -0,0 +1,26 @@
+public class loop{
+  public static void main(String[] args){
+  // // while loop start 
+  //   int i = 0;
+  //   while (i < 10){
+  //     System.out.println(i);
+  //     i++;
+  //   }
+  // // while loop end
+
+  // // for loop start
+
+  // for(int j = 0; j < 10; j++){
+  //   System.out.println(j);
+  // }
+  // // for loop end
+
+  //do while start
+  int i = 0;
+  do{
+    System.out.println(i); 
+    i = i + 1;
+  }while(i < 10);
+  //do while end
+  }
+}
